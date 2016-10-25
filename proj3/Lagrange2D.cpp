a2cb0f99352cd77cec042a937eed06b3dedfc9c2
Ben <bmaxey813@gmail.com>
2016-10-24 20:44:42 -0500

    Fix Lagrange interpolation

--- proj3/Lagrange2D.cpp
+++ proj3/Lagrange2D.cpp
@@ -1,20 +1,21 @@
 #include "Lagrange2D.h"
 #include "Lagrange.cpp"
+#include <iostream>
 
 double Lagrange2D(Matrix& x, Matrix& y, Matrix& f, double a, double b)
 {
 	double output = 0.0;
 
-	int m = x.Cols() - 1;
-	int n = y.Cols() - 1;
+	int m = x.Size() - 1;
+	int n = y.Size() - 1;
 
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i <= m; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j <= n; j++)
 		{
-			double xbasis = Lagrange_basis(x, m, a);
-			double ybasis = Lagrange_basis(y, n, b);
-			output += xbasis * ybasis * f(a, b);
+			double xbasis = Lagrange_basis(x, i, a);
+			double ybasis = Lagrange_basis(y, j, b);
+			output += xbasis * ybasis * f(i, j);
 		}
 	}
 
